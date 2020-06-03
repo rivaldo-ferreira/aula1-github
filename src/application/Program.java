4c1008474ee2f1db9637f98b2674e957d5daf3a3
Rivaldo Ferreira <rivaldo13@gmail.com>
2020-06-03 18:55:31 -0300

    Ultima mensagem

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Teste do GITHUB!");
 		System.out.println("Outra mensagem");
+		System.out.println();
+		System.out.println("Última mensagem");
 
 	}
 
